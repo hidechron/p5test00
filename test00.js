7fe4d4891ad5e606a33e38732767eb47272ba432
hide <hidechron@gmail.com>
2016-01-14 15:36:54 +0100

    added triangles and more

--- test00.js
+++ test00.js
@@ -1,50 +1,106 @@
+rnd = Math.random();
+var time = 0;
+var myPoints = [];
+var myTris = [];
+var curP = [0, 5, 14];
+var wait = 0;
+
 function setup() {
-  createCanvas(480, 270);
+  createCanvas(windowWidth, windowHeight);
   stroke(0);
   fill(150);
 
-  carrier = new p5.Oscillator(); // connects to master output by default
-  carrier.start();
-  carrier.freq(340);
-  carrier.amp(0.2);
-  // carrier's amp is 0 by default, giving our modulator total control
+  // INSTRUS ==========================================
+  drums = EDrums('x*o*x*o-', 1/4);
+  followD = Follow(drums);
 
+  a = Pluck();
+  a.play(Rndi(220, 880), 1/4);
+  followP = Follow(a);
 
-  modulator = new p5.Oscillator('triangle');
-  modulator.disconnect();  // disconnect the modulator from master output
-  modulator.start();
-  modulator.freq(5);
-  modulator.amp(1);
+  // CREATING POINTS ==================================
+  angleMode(DEGREES);
+  //strokeWeight(5);
+  for (var i=0; i<20; i++){
+    myPoints[i] = new pointr;
+  }
 
-  // Modulate the carrier's amplitude with the modulator
-  carrier.amp(modulator);
-
-  // create an fft to analyze the audio
-  fft = new p5.FFT();
+  // CREATING TRIS ====================================
+  for(var i=0; i<3; i++){
+    myTris[i] = new tris;
+  }
 }
 
+// ===================================================
+// ===================================================
 function draw() {
-  background(255);
-  rect(50,50,75,100);
+  //background(follow.getValue()*255)
+  background(30);
+  ellipseMode(RADIUS);
 
-  // // map mouseY to moodulator freq between 0 and 20hz
-  var modFreq = map(mouseY, 0, height, 4, 0);
-  modulator.freq(modFreq);
+  // TRIANGLES ========================================
+  var triscol = color(245, 245, 245, 25);
+  strokeWeight(0);
+  fill(triscol);
 
-  var modAmp = map(mouseX, 0, width, 0, 0.5);
-  modulator.amp(modAmp, 0.01); // fade time of 0.1 for smooth fading
+  wait -= 1;
 
-  // analyze the waveform
-  waveform = fft.waveform();
-
-  // draw the shape of the waveform
-  stroke(240);
-  strokeWeight(4);
-  beginShape();
-  for (var i = 0; i<waveform.length; i++){
-    var x = map(i, 0, waveform.length, 0, width);
-    var y = map(waveform[i], -1, 1, -height/2, height/2);
-    vertex(x, y + height/2);
+  // si le son des cordes est élevé changer de points pour les triangles.
+  if(followP.getValue() > 0.020 && wait < 0){
+    for(var i=0; i<curP.length; i++){
+      curP[i] = (curP[i] + 2) % (myPoints.length-3);
+    }
+    wait = 25;
   }
-  endShape();
+
+  for(var i=0; i<curP.length; i++){
+    console.log(curP[i]);
+    triangle(myPoints[curP[i]].posx, myPoints[curP[i]].posy,
+      myPoints[curP[i]+1].posx, myPoints[curP[i]+1].posy,
+      myPoints[curP[i]+2].posx, myPoints[curP[i]+2].posy);
+  }
+
+
+  // MOVING POINTS =====================================
+  strokeWeight(5);
+  fill(245);
+  for(var i=0; i<myPoints.length; i++){
+    pointSin.call(myPoints[i]);
+    ellipse(myPoints[i].posx, myPoints[i].posy, 5, 5);
+  }
+
+  time += 1;
+}
+
+// ====================================================
+// ====================================================
+
+function pointr() {
+  this.origx = 200 + Math.random()*(windowWidth-400);
+  this.origy = 200 + Math.random()*(windowHeight-400);
+  this.posx = this.origx;
+  this.posy = this.origy;
+  this.speed = 0.2;
+  this.angle = Math.random()*360;
+}
+
+var pointSin = function() {
+  this.speed = 0.2 + followD.getValue()*3;
+  this.angle += this.speed;
+
+  if(this.angle > 360){
+    this.angle = 0;
+  }
+
+  this.posx = windowWidth/2 + (this.origx-windowWidth/2) * cos(this.angle);
+  this.posy = windowHeight/2 + (this.origy-windowHeight/2) * cos(this.angle);
+}
+
+function tris(){
+  this.points = [floor(Math.random*20),floor(Math.random*20),floor(Math.random*20)];
+  this.wait = 15;
+}
+
+var trisChange = function(){
+
 }
